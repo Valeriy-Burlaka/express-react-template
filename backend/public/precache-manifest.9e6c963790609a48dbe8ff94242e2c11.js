40dfe2402585a3383ecaf302767dd8396c4bf07d
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2020-08-11 13:39:51 +0200

    initial commit

---
+++ backend/public/precache-manifest.9e6c963790609a48dbe8ff94242e2c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fbffbba092ec461f0c4cb2c9307551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d31dcd76ff7c13d6150d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4f72953a06012cfcdb5",
+    "url": "/static/js/2.ab9f1a32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab9f1a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31dcd76ff7c13d6150d",
+    "url": "/static/js/main.7c71bd51.chunk.js"
+  },
+  {
+    "revision": "65df3f03aa7bd845f89a",
+    "url": "/static/js/runtime-main.98b9b556.js"
+  }
+]);
